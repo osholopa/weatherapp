c5f30e64e6ca69135c34dd19adbe8094f0fa2631
Oskari Holopainen <oskariholopa@gmail.com>
2022-05-04 17:47:23 +0300

    Fix dev configuration

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -1,28 +1,28 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const GLOBALS = {
-  'process.env.ENDPOINT': JSON.stringify(process.env.ENDPOINT || 'http://0.0.0.0:9000/api'),
+  "process.env.ENDPOINT": JSON.stringify(
+    process.env.ENDPOINT || "http://0.0.0.0:9000/api"
+  ),
 };
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   cache: true,
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map",
   entry: {
-    main: ['@babel/polyfill', path.join(__dirname, 'src/index.jsx')],
+    main: ["@babel/polyfill", path.join(__dirname, "src/index.jsx")],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
-    modules: [
-      'src',
-      'node_modules',
-    ],
+    extensions: [".js", ".jsx"],
+    modules: ["src", "node_modules"],
   },
   devServer: {
-    static: 'src/public',
+    static: ['src/public'],
+    allowedHosts: 'all',
     historyApiFallback: true,
     host: process.env.HOST || '0.0.0.0',
     hot: true,
@@ -31,35 +31,32 @@ module.exports = {
     port: process.env.PORT || 8000,
   },
   output: {
-    filename: '[name].[hash:8].js',
-    publicPath: '/',
+    filename: "[name].[hash:8].js",
+    publicPath: "/",
   },
   module: {
     rules: [
       {
         test: /\.js$|\.jsx$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
       },
-      // Images
       {
-        test: /\.(?:ico|gif|png|svg|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-       // CSS and Sass
-       {
         test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/public/index.html',
-      filename: 'index.html',
+      template: "src/public/index.html",
+      filename: "index.html",
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin(GLOBALS),
   ],
+  watchOptions: {
+    poll: 1000,
+  },
 };
